2c085b1b2aa3a73c7b93d9c24c5e9d3641b0554c
Bac Nguyen <nxbac.dev@gmail.com>
2024-07-27 14:33:20 +0700

    lazygit

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.p10k.zsh ~/.p10k.zsh
-ln -s $PWD/nvim ~/.config/nvim
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+
+ln -s $PWD/nvim ~/.config/nvim
+ln -s $PWD/lazygit ~/.config/lazygit
